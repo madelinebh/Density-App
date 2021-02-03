857d0ce30bf702f9e1d4667bd6d2d0a6fa53800a
madelinebh <mrhea377@gmail.com>
2021-02-03 13:26:49 -0600

    Making boat Object

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+var boat;
+
 function setup() {
   createCanvas(400, 400);
+  boat = new boat(25, 300)
 }
 
 function draw() {
   background(220);
+  boat.show()
+  if (keyIsPressed === true) {
+      boat.sailAway();
+  }
 }
