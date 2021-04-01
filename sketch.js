520aa6712830d867dd3b624da9df27f527d36de4
madelinebh <mrhea377@gmail.com>
2021-04-01 13:25:13 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
+// formula = new Formula(315, 30)
+
+
 var boat;
+var formula; 
 
 function setup() {
   createCanvas(400, 400);
   boat = new Boat(25, 300)
+  formula = new Formula(315, 30)
 }
 
 function draw() {
   background(220);
   ocean();
+  formula.show()
   boat.show()
   if (keyIsPressed === true) {
       boat.sailAway();
